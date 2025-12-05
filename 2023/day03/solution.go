80f95856b5548497b80ae9900ec54d4420c3bc67
JanisVepris <janisvepris@gmail.com>
2025-12-05 22:46:12 +0200

    2023-03 a less acoustic solution

--- 2023/day03/solution.go
+++ 2023/day03/solution.go
@@ -2,170 +2,132 @@ package day03
 
 import (
 	"fmt"
-	"regexp"
-	"unicode"
+	"slices"
 
 	"janisvepris/aoc/internal/array"
 	"janisvepris/aoc/internal/conv"
 	"janisvepris/aoc/internal/files"
-	"janisvepris/aoc/internal/maths"
-)
-
-type Range struct {
-	start int
-	end   int
-}
-
-func (a *Range) overlap(b AdjacencyRange) bool {
-	return b.from <= a.end && a.start <= b.until
-}
-
-type AdjacencyRange struct {
-	from  int
-	until int
-}
-
-type Gear struct {
-	position int
-	iLine    int
-}
-
-func (g *Gear) adjacencyRange() AdjacencyRange {
-	return AdjacencyRange{from: maths.MaxInt(0, g.position-1), until: maths.MinInt(lineLen, g.position+2)}
-}
-
-type EnginePart struct {
-	value  int
-	iStart int
-	iEnd   int
-	iLine  int
-}
-
-func (p *EnginePart) getRange() Range {
-	return Range{p.iStart, p.iEnd}
-}
-
-func (p *EnginePart) adjacencyRange() AdjacencyRange {
-	return AdjacencyRange{from: maths.MaxInt(0, p.iStart-1), until: maths.MinInt(lineLen, p.iEnd+2)}
-}
-
-func (p *EnginePart) isValidPart() bool {
-	adjacencyRange := p.adjacencyRange()
-
-	part := lines[p.iLine][adjacencyRange.from:adjacencyRange.until]
-
-	if symbolsRe.MatchString(part) {
-		return true
-	}
-
-	if p.iLine > 0 {
-		part = lines[p.iLine-1][adjacencyRange.from:adjacencyRange.until]
-
-		if symbolsRe.MatchString(part) {
-			return true
-		}
-	}
-
-	if p.iLine < lineCount-1 {
-		part = lines[p.iLine+1][adjacencyRange.from:adjacencyRange.until]
-
-		if symbolsRe.MatchString(part) {
-			return true
-		}
-	}
-
-	return false
-}
-
-func createEnginePart(value, iStart, iEnd, iLine int) EnginePart {
-	return EnginePart{value, iStart, iEnd, iLine}
-}
-
-var (
-	lines       []string
-	lineLen     int
-	lineCount   int
-	engineParts []EnginePart
-	gears       []Gear
 )
 
 var (
-	numbersRe *regexp.Regexp = regexp.MustCompile(`^[0-9]$`)
-	symbolsRe *regexp.Regexp = regexp.MustCompile(`[^0-9\.]`)
+	lines   []string
+	offsets = [][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1}, // top
+		{0, -1}, {0, 1}, // middle
+		{1, -1}, {1, 0}, {1, 1}, // bottom
+	}
 )
 
 func Setup() {
 	lines = files.ReadFile("2023/day03/input.txt")
-	lineCount = len(lines)
-	lineLen = len(lines[0])
-	engineParts, gears = findParts()
 }
 
 func Part1() {
 	result := 0
+	number := ""
+	isValid := false
 
-	for _, enginePart := range engineParts {
-		if enginePart.isValidPart() {
-			result += enginePart.value
+	for row := range lines {
+		for col, ch := range lines[row] {
+			if ch < '0' || ch > '9' {
+				if isValid {
+					result += conv.StrToInt(number)
+					isValid = false
+				}
+				if number != "" {
+					number = ""
+				}
+				continue
+			}
+
+			number += string(ch)
+
+			for _, offset := range offsets {
+				nr := row + offset[0]
+				nc := col + offset[1]
+
+				if nr < 0 || nr >= len(lines[0]) || nc < 0 || nc >= len(lines) {
+					continue
+				}
+
+				neighbor := lines[nr][nc]
+
+				if (neighbor < '0' || neighbor > '9') && neighbor != '.' {
+					isValid = true
+					break
+				}
+			}
 		}
 	}
+
 	fmt.Printf("Part 1: %d\n", result)
 }
 
 func Part2() {
-	// TODO: Implement Part 2
 	result := 0
-	fmt.Printf("Part 2: %d\n", result)
-}
 
-func findParts() (engineParts []EnginePart, gears []Gear) {
-	numberScanning := false
-	numberStart := -1
-	numberEnd := -1
 	number := ""
+	isValid := false
+	gearRatios := map[uint64][]int{}
+	gears := [][2]int{}
 
-	for iLine, line := range lines {
-		for iChar, char := range line {
-			if unicode.IsDigit(char) {
+	for row := range lines {
+		for col, ch := range lines[row] {
+			if ch < '0' || ch > '9' {
+				if isValid {
+					isValid = false
 
-				if !numberScanning {
-					numberScanning = true
-					numberStart = iChar
+					for _, gear := range gears {
+						gearRatios[posToKey(gear)] = append(gearRatios[posToKey(gear)], conv.StrToInt(number))
+					}
 				}
-
-				number += string(char)
-				numberEnd = iChar
-
-				if iChar == lineLen-1 {
-					numberScanning = false
-					engineParts = append(engineParts, createEnginePart(conv.StrToInt(number), numberStart, numberEnd, iLine))
-
-					numberStart = -1
-					numberEnd = -1
+				if number != "" {
 					number = ""
 				}
-			} else if numberScanning {
-				numberScanning = false
-				engineParts = append(engineParts, createEnginePart(conv.StrToInt(number), numberStart, numberEnd, iLine))
 
-				numberStart = -1
-				numberEnd = -1
-				number = ""
+				if len(gears) > 0 {
+					gears = [][2]int{}
+				}
+
+				continue
 			}
 
-			if char == '*' {
-				gears = append(gears, Gear{position: iChar, iLine: iLine})
-			}
+			number += string(ch)
 
+			for _, offset := range offsets {
+				nr := row + offset[0]
+				nc := col + offset[1]
+
+				if nr < 0 || nr >= len(lines[0]) || nc < 0 || nc >= len(lines) {
+					continue
+				}
+
+				neighbor := lines[nr][nc]
+
+				if (neighbor < '0' || neighbor > '9') && neighbor != '.' {
+					isValid = true
+				}
+
+				if neighbor == '*' {
+					if !slices.Contains(gears, [2]int{nr, nc}) {
+						array.Push(&gears, [2]int{nr, nc})
+					}
+				}
+			}
 		}
 	}
 
-	return
+	for _, ratios := range gearRatios {
+		if len(ratios) != 2 {
+			continue
+		}
+
+		result += ratios[0] * ratios[1]
+	}
+
+	fmt.Printf("Part 2: %d\n", result)
 }
 
-func getPartsInLine(iLine int) []EnginePart {
-	return array.Filter(
-		engineParts,
-		func(p EnginePart) bool { return p.iLine == iLine && p.isValidPart() },
-	)
+func posToKey(pos [2]int) uint64 {
+	return uint64(pos[0])<<32 | uint64(pos[1])
 }
